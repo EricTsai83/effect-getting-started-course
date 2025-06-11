83947913a5c53f03396bd329fd404a91b8f234cf
Eric Tsai <eric492718@gmail.com>
2025-06-12 02:09:06 +0800

    Define schema with Schema.Class

---
+++ src/index-gen.ts
@@ -0,0 +1,52 @@
+import { Data, Effect } from "effect";
+import { Schema } from "effect";
+
+class Pokemon extends Schema.Class<Pokemon>("Pokemon")({
+  id: Schema.Number,
+  order: Schema.Number,
+  name: Schema.String,
+  height: Schema.Number,
+  weight: Schema.Number,
+}) {}
+
+/** Errors **/
+class FetchError extends Data.TaggedError("FetchError")<{}> {}
+class JsonError extends Data.TaggedError("JsonError")<{}> {}
+
+/** Implementation **/
+const fetchRequest = Effect.tryPromise({
+  try: () => fetch("https://pokeapi.co/api/v2/pokemon/garchomp/"),
+  catch: () => new FetchError(),
+});
+
+const jsonResponse = (response: Response) =>
+  Effect.tryPromise({
+    try: () => response.json(),
+    catch: () => new JsonError(),
+  });
+
+const decodePokemon = Schema.decodeUnknown(Pokemon);
+
+/** Core implementation **/
+const program = Effect.gen(function* () {
+  const response = yield* fetchRequest;
+  if (!response.ok) {
+    return yield* new FetchError();
+  }
+
+  const json = yield* jsonResponse(response);
+
+  return yield* decodePokemon(json);
+});
+
+/** Error handling **/
+const main = program.pipe(
+  Effect.catchTags({
+    FetchError: () => Effect.succeed("Fetch error"),
+    JsonError: () => Effect.succeed("Json error"),
+    ParseError: () => Effect.succeed("Parse error"),
+  }),
+);
+
+/** Running effect **/
+Effect.runPromise(main).then(console.log);
---
+++ src/index-pipe.ts
@@ -0,0 +1,30 @@
+import { Effect, Data } from "effect";
+
+class FetchError extends Data.TaggedError("FetchError")<Readonly<{}>> {}
+
+class JsonError extends Data.TaggedError("JsonError")<Readonly<{}>> {}
+
+const fetchRequest = Effect.tryPromise({
+  try: () => fetch("https://pokeapi.co/api/v2/psadokemon/garchomp/"),
+  catch: () => new FetchError(),
+});
+
+const jsonResponse = (response: Response) =>
+  Effect.tryPromise({
+    try: () => response.json(),
+    catch: () => new JsonError(),
+  });
+
+const main = fetchRequest.pipe(
+  Effect.filterOrFail(
+    (response) => response.ok,
+    () => new FetchError(),
+  ),
+  Effect.flatMap(jsonResponse),
+  Effect.catchTags({
+    FetchError: () => Effect.succeed("Fetch error"),
+    JsonError: () => Effect.succeed("Json error"),
+  }),
+);
+
+Effect.runPromise(main).then(console.log);
