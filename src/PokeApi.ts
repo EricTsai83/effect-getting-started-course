e6f71fc09db3c15ca08967da992e543aea1ef8c5
Eric Tsai <eric492718@gmail.com>
2025-06-16 01:05:53 +0800

    Dependencies on the service level

--- src/PokeApi.ts
+++ src/PokeApi.ts
@@ -1,34 +1,43 @@
-import { Effect, Context, Schema } from "effect";
+import { Effect, Context, Schema, Layer } from "effect";
 import { FetchError, JsonError } from "./errors";
 import { Pokemon } from "./schemas";
 import { PokemonCollection } from "./PokemonCollection";
 import { BuildPokeApiUrl } from "./BuildPokeApiUrl";
 
-const make = {
-  getPokemon: Effect.gen(function* () {
-    const pokemonCollection = yield* PokemonCollection;
-    const buildPokeApiUrl = yield* BuildPokeApiUrl;
+const make = Effect.gen(function* () {
+  /// 1️⃣ Extract `PokemonCollection` and `BuildPokeApiUrl` outside of `getPokemon`
+  const pokemonCollection = yield* PokemonCollection;
+  const buildPokeApiUrl = yield* BuildPokeApiUrl;
 
-    const requestUrl = buildPokeApiUrl({ name: pokemonCollection[0] });
+  return {
+    getPokemon: Effect.gen(function* () {
+      const requestUrl = buildPokeApiUrl({ name: pokemonCollection[0] });
 
-    const response = yield* Effect.tryPromise({
-      try: () => fetch(requestUrl),
-      catch: () => new FetchError(),
-    });
+      const response = yield* Effect.tryPromise({
+        try: () => fetch(requestUrl),
+        catch: () => new FetchError(),
+      });
 
-    if (!response.ok) {
-      return yield* new FetchError();
-    }
+      if (!response.ok) {
+        return yield* new FetchError();
+      }
 
-    const json = yield* Effect.tryPromise({
-      try: () => response.json(),
-      catch: () => new JsonError(),
-    });
+      const json = yield* Effect.tryPromise({
+        try: () => response.json(),
+        catch: () => new JsonError(),
+      });
 
-    return yield* Schema.decodeUnknown(Pokemon)(json);
-  }),
-};
+      return yield* Schema.decodeUnknown(Pokemon)(json);
+    }),
+  };
+});
 
-export class PokeApi extends Context.Tag("PokeApi")<PokeApi, typeof make>() {
-  static readonly Live = PokeApi.of(make);
+export class PokeApi extends Context.Tag("PokeApi")<
+  PokeApi,
+  Effect.Effect.Success<typeof make>
+>() {
+  static readonly Live = Layer.effect(this, make).pipe(
+    // 👇 Remember: provide dependencies directly inside `Live`
+    Layer.provide(Layer.mergeAll(PokemonCollection.Live, BuildPokeApiUrl.Live)),
+  );
 }
